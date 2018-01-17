5883a21c8474c3661ded022333df2fd6f8f5ffda
Markus GH <markus.ghambari@gmail.com>
2018-01-17 12:26:58 +1100

    Create checkdaemon.sh

---
+++ checkdaemon.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# checkdaemon.sh
+# Make sure the daemon is not stuck.
+# Add the following to the crontab (i.e. crontab -e)
+# */30 * * * * ~/smartnode/checkdaemon.sh
+
+previousBlock=$(cat ~/smartnode/blockcount)
+currentBlock=$(smartcash-cli getblockcount)
+
+smartcash-cli getblockcount > ~/smartnode/blockcount
+
+if [ "$previousBlock" == "$currentBlock" ]; then
+  smartcash-cli stop
+  smartcashd
+fi
