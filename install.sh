e4a66cadc6b8b2096a8fe56fa5ab3222a7a79f50
controllinghand <31561227+controllinghand@users.noreply.github.com>
2018-01-25 16:23:01 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,12 @@
 #!/bin/bash
 # install.sh
-# Installs smartnode on Ubuntu 17.10 x64
+# Checks smartnode on Ubuntu 17.10 x64
 # ATTENTION: The anti-ddos part will disable http, https and dns ports.
 
 if [ "$(whoami)" != "root" ]; then
   echo "Script must be run as user: root"
   exit -1
 fi
-
-while true; do
- if [ -d ~/.smartcash ]; then
-   printf "~/.smartcash/ already exists! The installer will delete this folder. Continue anyway?(Y/n)"
-   read REPLY
-   if [ ${REPLY} == "Y" ]; then
-      pID=$(ps -ef | grep smartcashd | awk '{print $2}')
-      kill ${pID}
-      rm -r ~/.smartcash/
-      break
-   else
-      if [ ${REPLY} == "n" ]; then
-        exit
-      fi
-   fi
- else
-   break
- fi
-done
 
 # Warning that the script will reboot the server
 echo "WARNING: This script will reboot the server when it's finished."
@@ -34,50 +15,9 @@ read IGNORE
 
 cd
 # Changing the SSH Port to a custom number is a good security measure against DDOS attacks
-printf "Custom SSH Port(Enter to ignore): "
-read VARIABLE
-_sshPortNumber=${VARIABLE:-22}
-
-# Get a new privatekey by going to console >> debug and typing smartnode genkey
-printf "SmartNode GenKey: "
-read _nodePrivateKey
-
-# The RPC node will only accept connections from your localhost
-_rpcUserName=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12 ; echo '')
-
-# Choose a random and secure password for the RPC
-_rpcPassword=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 ; echo '')
-
-# Get the IP address of your vps which will be hosting the smartnode
-_nodeIpAddress=$(ip route get 1 | awk '{print $NF;exit}')
-
-# Make a new directory for smartcash daemon
-mkdir ~/.smartcash/
-touch ~/.smartcash/smartcash.conf
-
-# Change the directory to ~/.smartcash
-cd ~/.smartcash/
-
-# Create the initial smartcash.conf file
-echo "rpcuser=${_rpcUserName}
-rpcpassword=${_rpcPassword}
-rpcallowip=127.0.0.1
-listen=1
-server=1
-daemon=1
-logtimestamps=1
-maxconnections=64
-txindex=1
-smartnode=1
-externalip=${_nodeIpAddress}:9678
-smartnodeprivkey=${_nodePrivateKey}
-" > smartcash.conf
-cd
-
-# Install smartcashd using apt-get
-apt-get update -y
-apt-get install software-properties-common
-add-apt-repository ppa:smartcash/ppa -y && apt update && apt install smartcashd -y && smartcashd
+# printf "Custom SSH Port(Enter to ignore): "
+# read VARIABLE
+# _sshPortNumber=${VARIABLE:-22}
 
 # Create a directory for smartnode's cronjobs and the anti-ddos script
 rm -r smartnode
@@ -87,11 +27,11 @@ mkdir smartnode
 cd ~/smartnode/
 
 # Download the appropriate scripts
-wget https://raw.githubusercontent.com/SmartCash/smartnode/master/anti-ddos.sh
-wget https://raw.githubusercontent.com/SmartCash/smartnode/master/makerun.sh
-wget https://raw.githubusercontent.com/SmartCash/smartnode/master/checkdaemon.sh
-wget https://raw.githubusercontent.com/SmartCash/smartnode/master/upgrade.sh
-wget https://raw.githubusercontent.com/SmartCash/smartnode/master/clearlog.sh
+wget https://github.com/controllinghand/smartnode-checkonly/master/anti-ddos.sh
+wget https://github.com/controllinghand/smartnode-checkonly/master/makerun.sh
+wget https://github.com/controllinghand/smartnode-checkonly/master/checkdaemon.sh
+wget https://github.com/controllinghand/smartnode-checkonly/master/upgrade.sh
+wget https://github.com/controllinghand/smartnode-checkonly/master/clearlog.sh
 
 # Create a cronjob for making sure smartcashd is always running
 (crontab -l ; echo "*/1 * * * * ~/smartnode/makerun.sh") | crontab -
@@ -110,8 +50,8 @@ chmod 0700 ./upgrade.sh
 chmod 0700 ./clearlog.sh
 
 # Change the SSH port
-sed -i "s/[#]\{0,1\}[ ]\{0,1\}Port [0-9]\{2,\}/Port ${_sshPortNumber}/g" /etc/ssh/sshd_config
-sed -i "s/14855/${_sshPortNumber}/g" ~/smartnode/anti-ddos.sh
+# sed -i "s/[#]\{0,1\}[ ]\{0,1\}Port [0-9]\{2,\}/Port ${_sshPortNumber}/g" /etc/ssh/sshd_config
+# sed -i "s/14855/${_sshPortNumber}/g" ~/smartnode/anti-ddos.sh
 
 # Run the anti-ddos script
 bash ./anti-ddos.sh
