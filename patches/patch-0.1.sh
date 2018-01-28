fd1ab548a8773c66414f57d848dae1926d59a00c
Markus GH <markus.ghambari@gmail.com>
2018-01-28 11:41:03 +1100

    Update patch-0.1.sh

--- patches/patch-0.1.sh
+++ patches/patch-0.1.sh
@@ -5,7 +5,11 @@ cd ~/smartnode
 wget https://raw.githubusercontent.com/SmartCash/smartnode/master/clearlog.sh 
 
 # Create a cronjob for clearing the log file
-(crontab -l ; echo "0 0 */2 * * ~/smartnode/clearlog.sh") | crontab -
+if ! crontab -l | grep "~/smartnode/clearlog.sh"; then
+  (crontab -l ; echo "0 0 */2 * * ~/smartnode/clearlog.sh") | crontab -
+fi
+
+# Give execute permission to the cron script
 chmod 0700 ./clearlog.sh
 
 ./clearlog.sh
