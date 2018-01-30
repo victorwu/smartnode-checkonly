9eaaa8bc8000f2fff83ca568377132817d5a15cb
Victor Wu <victorwu@users.noreply.github.com>
2018-01-29 23:56:54 -0800

    install.sh for users who manually installed
    Optional: Uncomment to change ssh ports (line: 20, 60, 61) and maxconnections (line: 55 - 57)

--- install.sh
+++ install.sh
@@ -1,31 +1,12 @@
 #!/bin/bash
 # install.sh
-# Installs smartnode on Ubuntu 16.04 LTS x64
+# Checks smartnode on Ubuntu 17.10 x64
 # ATTENTION: The anti-ddos part will disable http, https and dns ports.
 
-if [ "$(whoami)" != "root" ]; then
-  echo "Script must be run as user: root"
+if [ "$(whoami)" == "root" ]; then
+  echo "Script must be run as user: smartadmin or ID you setup for smartcash"
   exit -1
 fi
-
-while true; do
- if [ -d ~/.smartcash ]; then
-   printf "~/.smartcash/ already exists! The installer will delete this folder. Continue anyway?(Y/n)"
-   read REPLY
-   if [ ${REPLY} == "Y" ]; then
-      pID=$(ps -ef | grep smartcashd | awk '{print $2}')
-      kill ${pID}
-      rm -r ~/.smartcash/
-      break
-   else
-      if [ ${REPLY} == "n" ]; then
-        exit
-      fi
-   fi
- else
-   break
- fi
-done
 
 # Warning that the script will reboot the server
 echo "WARNING: This script will reboot the server when it's finished."
@@ -34,50 +15,9 @@ read IGNORE
 
 cd
 # Changing the SSH Port to a custom number is a good security measure against DDOS attacks
-printf "Custom SSH Port(Enter to ignore): "
-read VARIABLE
-_sshPortNumber=${VARIABLE:-22}
-
-# Get a new privatekey by going to console >> debug and typing smartnode genkey
-printf "SmartNode GenKey: "
-read _nodePrivateKey
-
-# The RPC node will only accept connections from your localhost
-_rpcUserName=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12 ; echo '')
-
-# Choose a random and secure password for the RPC
-_rpcPassword=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 ; echo '')
-
-# Get the IP address of your vps which will be hosting the smartnode
-_nodeIpAddress=$(ip route get 1 | awk '{print $NF;exit}')
-
-# Make a new directory for smartcash daemon
-mkdir ~/.smartcash/
-touch ~/.smartcash/smartcash.conf
-
-# Change the directory to ~/.smartcash
-cd ~/.smartcash/
-
-# Create the initial smartcash.conf file
-echo "rpcuser=${_rpcUserName}
-rpcpassword=${_rpcPassword}
-rpcallowip=127.0.0.1
-listen=1
-server=1
-daemon=1
-logtimestamps=1
-maxconnections=64
-txindex=1
-smartnode=1
-externalip=${_nodeIpAddress}:9678
-smartnodeprivkey=${_nodePrivateKey}
-" > smartcash.conf
-cd
-
-# Install smartcashd using apt-get
-apt-get update -y
-apt-get install software-properties-common
-add-apt-repository ppa:smartcash/ppa -y && apt update && apt install smartcashd -y && smartcashd
+# printf "Custom SSH Port(Enter to ignore): "
+# read VARIABLE
+# _sshPortNumber=${VARIABLE:-22}
 
 # Create a directory for smartnode's cronjobs and the anti-ddos script
 rm -r smartnode
@@ -94,37 +34,37 @@ wget https://raw.githubusercontent.com/SmartCash/smartnode/master/upgrade.sh
 wget https://raw.githubusercontent.com/SmartCash/smartnode/master/clearlog.sh
 
 # Create a cronjob for making sure smartcashd is always running
-if ! crontab -l | grep "~/smartnode/makerun.sh"; then
-  (crontab -l ; echo "*/1 * * * * ~/smartnode/makerun.sh") | crontab -
-fi
+# SLG changed to every 10 mins to help keep it from stepping on setting up nodes and upgrades etc
+(crontab -l ; echo "*/1 * * * * ~/smartnode/makerun.sh") | crontab -
+chmod 0700 ./makerun.sh
 
 # Create a cronjob for making sure the daemon is never stuck
-if ! crontab -l | grep "~/smartnode/checkdaemon.sh"; then
-  (crontab -l ; echo "*/30 * * * * ~/smartnode/checkdaemon.sh") | crontab -
-fi
+(crontab -l ; echo "*/30 * * * * ~/smartnode/checkdaemon.sh") | crontab -
+chmod 0700 ./checkdaemon.sh
 
 # Create a cronjob for making sure smartcashd is always up-to-date
-if ! crontab -l | grep "~/smartnode/upgrade.sh"; then
-  (crontab -l ; echo "*/120 * * * * ~/smartnode/upgrade.sh") | crontab -
-fi
+(crontab -l ; echo "*/120 * * * * ~/smartnode/upgrade.sh") | crontab -
+chmod 0700 ./upgrade.sh
 
 # Create a cronjob for clearing the log file
-if ! crontab -l | grep "~/smartnode/clearlog.sh"; then
-  (crontab -l ; echo "0 0 */2 * * ~/smartnode/clearlog.sh") | crontab -
-fi
-
-# Give execute permission to the cron scripts
-chmod 0700 ./makerun.sh
-chmod 0700 ./checkdaemon.sh
-chmod 0700 ./upgrade.sh
+(crontab -l ; echo "0 0 */2 * * ~/smartnode/clearlog.sh") | crontab -
 chmod 0700 ./clearlog.sh
 
-# Change the SSH port
-sed -i "s/[#]\{0,1\}[ ]\{0,1\}Port [0-9]\{2,\}/Port ${_sshPortNumber}/g" /etc/ssh/sshd_config
-sed -i "s/14855/${_sshPortNumber}/g" ~/smartnode/anti-ddos.sh
+/etc/init.d/cron reload
 
-# Run the anti-ddos script
-bash ./anti-ddos.sh
+# smartcash-cli stop
+# pico ~/.smartcash/smartcash.conf
+# maxconnections=64
+
+# Change the SSH port
+# sed -i "s/[#]\{0,1\}[ ]\{0,1\}Port [0-9]\{2,\}/Port ${_sshPortNumber}/g" /etc/ssh/sshd_config
+# sed -i "s/14855/${_sshPortNumber}/g" ~/smartnode/anti-ddos.sh
+
+# Run the anti-ddos script You will need to put in your sudo password
+sudo bash ./anti-ddos.sh
 
 # Reboot the server
-reboot
+echo "Rebooting the server so that changes will take effect."
+printf "Enter to continue: "
+read IGNORE
+sudo reboot
